784e60491e321ce069ec67ccbc4966974010f430
Musharraf Tahir <musharraf.tahir@devsinc.com>
2026-02-23 20:11:11 +0500

    Moved Queue error to main errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -6,3 +6,6 @@ var ErrRetryable = errors.New("retryable error")
 
 var ErrFatal = errors.New("fatal error")
 
+var ErrMissingID =  errors.New("task is is required")
+
+var ErrMissingKind = errors.New("task kind is required")
--- internal/queue/validate.go
+++ internal/queue/validate.go
@@ -1,19 +1,17 @@
 package queue
 
 import (
-	"errors"
 	"goq/internal/domain"
 )
 
-var ErrMissingD =  errors.New("task is is required")
-var ErrMissingKind = errors.New("task kind is required")
+
 
 func Validate(task *domain.Task) error {
 	if task.ID == "" {
-		return ErrMissingD 
+		return domain.ErrMissingID 
 	}
 	if task.Kind == "" {
-		return ErrMissingKind
+		return domain.ErrMissingKind
 	}
 	return nil
 }
