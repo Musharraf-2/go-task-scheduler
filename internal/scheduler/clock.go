59acd93126ef6180df1569089b549917a51188fe
Musharraf Tahir <musharraf.tahir@devsinc.com>
2026-03-02 15:56:43 +0500

    Added clock interface

---
+++ internal/scheduler/clock.go
@@ -0,0 +1,13 @@
+package scheduler
+
+import "time"
+
+// Clock is injectable so tests can advance time without sleeping.
+type Clock interface {
+	Now() time.Time
+}
+
+// RealClock uses the system clock.
+type RealClock struct{}
+
+func (RealClock) Now() time.Time { return time.Now() }
